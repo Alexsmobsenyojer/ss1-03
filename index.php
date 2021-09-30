47c319676e55beaf03e2d2cc360fd00d829cceaa
Alexsmobsenyojer <trejbal.jakub@email.cz>
2021-09-30 15:42:17 +0200

    a

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once "vendor".DIRECTORY_SEPARATOR."autoload.php";
-$Names = ["Sorin Eni","Štěpán Kolarovský","Karel Nakládal","Thanh Hieu Ngo","Michal Plaček","Šimon Siksta","Jan Šlechta","Jakub Vávrů"];
-$Emails = ["a@student.ossp.cz","b@student.ossp.cz","c@student.ossp.cz","d@student.ossp.cz","e@student.ossp.cz","f@student.ossp.cz","g@student.ossp.cz","h@student.ossp.cz"];
+$Names = ["Jakub Trejbal","Sorin Eni","Štěpán Kolarovský","Karel Nakládal","Thanh Hieu Ngo","Michal Plaček","Šimon Siksta","Jan Šlechta","Jakub Vávrů"];
+$Emails = ["jakub.trejbal@student.ossp.cz","a@student.ossp.cz","b@student.ossp.cz","c@student.ossp.cz","d@student.ossp.cz","e@student.ossp.cz","f@student.ossp.cz","g@student.ossp.cz","h@student.ossp.cz"];
 
 foreach (array_combine($Names,$Emails) as $name => $email) {
     $mail = new Nette\Mail\Message;
